d1e131bc6115a0668470ce4ead4eb9f4a7136106
Hoki <1714043771@qq.com>
2018-09-26 00:52:40 +0800

    rectify main.js

--- static/js/main.js
+++ static/js/main.js
@@ -1,17 +1,18 @@
 main();
 function main() {
-    var timer;
-    function wink(){
-        var interval;
-       function change(){
-            element=document.getElementById('myIcon')
-            if (element.src.match("icon")) {
-                element.src = "/static/img/icon3.png";
-            } else {
-                element.src = "/static/img/icon.png";
-            }
+    let timer;
+    let frame = 0;
+
+    function wink() {
+        let element;
+        element = document.getElementById('myIcon');
+        if (0 === frame % 30) {
+            element.src = "/static/img/icon3.png";
+            frame = 0;
+        } else {
+            element.src = "/static/img/icon.png";
         }
-        interval = setInterval(change,600);
+        frame++;
     }
-    timer = setInterval(wink, 1000);
+    timer = setInterval(wink, 200);
 }
